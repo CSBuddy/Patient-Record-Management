107d15665c472653644a409f9388f7ff7bf9f5e7
Saish Mhatre <saish.mhatre26@gmail.com>
2019-10-16 20:02:47 +0530

    added more entities in database & added multiple html files

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
 class Patient(models.Model):
+    patient_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     mobile = models.BigIntegerField()
@@ -9,6 +11,25 @@ class Patient(models.Model):
     address = models.CharField(max_length=255)
     occupation = models.CharField(max_length=20)
 
+    contineous = models.CharField(max_length=50, blank=True, null=True)
+    type = models.CharField(max_length=50, blank=True, null=True)
+    aggravating = models.CharField(max_length=50, blank=True, null=True)
+    relieving = models.CharField(max_length=50, blank=True, null=True)
+    duration = models.CharField(max_length=50, blank=True, null=True)
+
+    disease = models.CharField(max_length=20, blank=True, null=True)
+    recent_wt_loss = models.IntegerField(blank=True, null=True)
+    breathing_pattern = models.CharField(max_length=20, blank=True, null=True)
+    heal_walking = models.CharField(max_length=20, blank=True, null=True)
+    
+    morning_stiffness = models.CharField(max_length=50, blank=True, null=True)
+    numbness = models.CharField(max_length=50, blank=True, null=True)
+    mechanical_injury = models.CharField(max_length=50, blank=True, null=True)
+    mascular_weakness = models.CharField(max_length=50, blank=True, null=True)
+    sleeping_disturbance = models.CharField(max_length=50, blank=True, null=True)
+    mode_of_onset = models.CharField(max_length=50, blank=True, null=True)
+    claudication = models.CharField(max_length=50, blank=True, null=True)
+
     class Meta:
-        managed = True
+        managed = False
         db_table = 'patient'
